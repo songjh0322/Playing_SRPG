815b03038ea1dfd94478939bb2bc7f5e9c6aa27e
Songjh0322 <happymine0322@naver.com>
2024-09-03 21:58:46 +0900

    PlayerStats

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int hp = 100;
+    public int att = 20;
+    public int def = 10;
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Debug.Log(gameObject.name + "이 사망하였습니다.");
+
+        }
+    }
+}
