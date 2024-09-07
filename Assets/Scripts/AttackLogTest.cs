5ec0bf7861420611af0be42849f89ec44b12d6f9
MaBeing <john9912@naver.com>
2024-09-07 17:31:05 +0900

    Sample Scene 오브젝트 Collider을 크기에 맞게 수정

--- Assets/Scripts/AttackLogTest.cs
+++ Assets/Scripts/AttackLogTest.cs
@@ -13,15 +13,19 @@ public class AttackLogTest : MonoBehaviour
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
-            Debug.Log("여기까지 진행됨!");
+            // Ray 시각적 디버깅
+            Debug.DrawRay(mousePosition, Vector2.zero, Color.red, 1.0f);
 
             // Collider가 있는지 확인
             if (hit.collider != null)
             {
-                // 클릭한 오브젝트가 Box인지 확인
-                if (hit.collider.gameObject == gameObject)
+                // 클릭한 오브젝트의 Collider 확인
+                //Debug.Log("Collider가 감지되었습니다.");
+
+                // 태그가 "Player"인지 확인
+                if (hit.collider.CompareTag("Player"))
                 {
-                    Debug.Log("Box clicked!");
+                    Debug.Log("Player 오브젝트가 클릭되었습니다.");
                 }
             }
         }
