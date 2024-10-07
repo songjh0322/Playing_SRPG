86e2c0351d28348113a9f9650a366c8b535036f9
MaBeing <john9912@naver.com>
2024-10-07 14:50:03 +0900

    배치 화면에서 캐릭터 이름 불러오기 구현

--- Assets/Scripts/Managers/DeployManager.cs
+++ Assets/Scripts/Managers/DeployManager.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 // 유닛 배치를 관리하는 매니저
 public class DeployManager : MonoBehaviour
@@ -21,11 +23,39 @@ public class DeployManager : MonoBehaviour
         }
     }
 
+    GameObject characterButton;
+
+    List<Button> characterButtons;
+
+    // 컴포넌트
+    TextMeshProUGUI textMeshPro;
+    TMP_FontAsset hangeulFont;
+
     private void Start()
     {
-        // 플레이어 선택한 유닛들을 사용
+        // 플레이어가 선택한 유닛들을 설정하고 AI 플레이어가 maxUnits(6)개의 유닛을 선택함
         UnitManager.Instance.ConfirmPlayer1Units(CharacterSelectionManager.Instance.selectedCharacters);
         UnitManager.Instance.RandomizePlayer2Units();
+
+        // 더이상 사용하지 않는 CharacterSelectionManager를 삭제
+        GameObject characterSelectionManager = GameObject.Find("@CharacterSelectionManager");
+        if (characterSelectionManager != null)
+            Destroy(characterSelectionManager);
+
+        hangeulFont = Resources.Load<TMP_FontAsset>("Fonts/Orbit-Regular SDF");
+
+        // 게임 오브젝트 지정 (이름으로 찾음)
+        characterButton = GameObject.Find("CharacterButtons");
+
+        // 모든 캐릭터 버튼에 이름을 표기
+        for (int i = 0; i < 6; i++)
+        {
+            Transform characterButtonTransform = characterButton.transform.GetChild(i);
+            Transform textTransform = characterButtonTransform.Find("Text (TMP)");
+            TMP_Text textMeshPro = textTransform.GetComponent<TMP_Text>();
+            textMeshPro.text = UnitManager.Instance.player1Units[i].basicStats.unitName;
+            textMeshPro.font = hangeulFont;
+        }
     }
 
 
