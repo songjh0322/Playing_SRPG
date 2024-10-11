291bb61fcd7271e43c8c1df511db2387aca82fee
Songjh0322 <happymine0322@naver.com>
2024-10-11 15:04:27 +0900

    캐릭터 목록 텍스트 출력

--- Assets/Scripts/TestScripts/GameManager2.cs
+++ Assets/Scripts/TestScripts/GameManager2.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager2 : MonoBehaviour
 {
     public static GameManager2 instance;  // 싱글톤 패턴으로 모든 씬에서 GamaManager에 쉽게 접근할 수 있도록 함.
+
+    public Text characterNameText; //캐릭터 이름을 출력할 텍스트 UI
     private void Awake()
     {
         //싱글톤 패턴
@@ -30,6 +33,16 @@ public class GameManager2 : MonoBehaviour
         Debug.Log("게임 종료");
         Application.Quit();
     }
-        
+
+    //캐릭터 이름을 출력하는 함수
+    public void SetCharacterName(string characterName)
+    {
+        if (characterNameText != null)
+        {
+            characterNameText.text = characterName; //해당 UI에 캐릭터 이름 출력
+            Debug.Log("선택된 캐릭터: " + characterName);
+        }
+    }
+
 
 }
