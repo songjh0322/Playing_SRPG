5105d84db68e328fcd294ed4f82e7fe4a7c2219c
Songjh0322 <happymine0322@naver.com>
2024-10-11 18:28:37 +0900

    선택된 캐릭터 정보 삭제 기능 추가

--- Assets/Scripts/TestScripts/GameManager2.cs
+++ Assets/Scripts/TestScripts/GameManager2.cs
@@ -15,6 +15,8 @@ public class GameManager2 : MonoBehaviour
     public Image[] selectedCharacterImages; // 선택된 캐릭터 정보를 표시할 Content 밑의 5개의 Image 오브젝트
     public Text[] selectedCharacterTexts; // 선택된 캐릭터 이름을 표시할 Text 오브젝트 배열 (Image에 붙은 Text)
     public Image[] characterButtons; // 캐릭터 버튼들의 Image 배열 (각각의 주황색 버튼을 관리)
+    public Button[] DeleteButtons; // 삭제 버튼 배열
+
 
     private int currentSelectedIndex = -1; // 현재 선택된 캐릭터의 인덱스 (-1은 선택되지 않은 상태)
     private string currentSelectedName = ""; // 선택된 캐릭터의 이름
@@ -58,6 +60,7 @@ public class GameManager2 : MonoBehaviour
         {
             selectedCharacterImages[i].enabled = false;  // 이미지 비활성화
             selectedCharacterTexts[i].text = "";         // 텍스트 초기화
+            DeleteButtons[i].gameObject.SetActive(false); // 삭제 버튼 비활성화
         }
     }
 
@@ -105,6 +108,27 @@ public class GameManager2 : MonoBehaviour
             }
         }
     }
+    // 캐릭터 삭제 함수
+    public void RemoveCharacter(int index)
+    {
+        if (index >= 0 && index < selectedCharacters.Count)
+        {
+            Debug.Log("캐릭터 삭제: " + selectedCharacters[index]);
+
+            // 버튼 색상 초기화
+            for (int i = 0; i < characterButtons.Length; i++)
+            {
+                // 캐릭터 이름이 동일하면 해당 캐릭터 버튼의 색상만 초기화
+                if (characterButtons[i].transform.Find("Text").GetComponent<Text>().text == selectedCharacters[index])
+                {
+                    characterButtons[i].color = new Color32(190, 104, 69, 255);  // 원래 색상으로 변경
+                    break;
+                }
+            }
+            selectedCharacters.RemoveAt(index);  // 리스트에서 해당 캐릭터 삭제
+            UpdateSelectedCharactersUI();        // UI 업데이트
+        }
+    }
 
     // 선택된 캐릭터들을 UI에 업데이트하는 함수
     private void UpdateSelectedCharactersUI()
@@ -116,12 +140,28 @@ public class GameManager2 : MonoBehaviour
                 // 선택된 캐릭터 정보가 있을 경우, 이미지와 텍스트 업데이트
                 selectedCharacterTexts[i].text = selectedCharacters[i];  // 이름 업데이트
                 selectedCharacterImages[i].enabled = true;  // 이미지 활성화
+                DeleteButtons[i].gameObject.SetActive(true); // 삭제 버튼 활성화
+
+                // 삭제 버튼의 OnClick 이벤트 설정
+                int index = i;  // 로컬 변수로 i 저장
+                DeleteButtons[i].onClick.RemoveAllListeners();  // 기존 리스너 제거
+                DeleteButtons[i].onClick.AddListener(() => RemoveCharacter(index));  // 삭제 함수 호출 연결
             }
             else
             {
                 // 선택되지 않은 칸은 비워두거나 비활성화
                 selectedCharacterTexts[i].text = "";
                 selectedCharacterImages[i].enabled = false;  // 이미지 비활성화
+                DeleteButtons[i].gameObject.SetActive(false); // 삭제 버튼 비활성화
+            }
+        }
+
+        // 선택된 캐릭터의 텍스트와 버튼이 일치하는지 확인하고 색상 초기화
+        for (int i = 0; i < characterButtons.Length && i < selectedCharacterTexts.Length; i++)
+        {
+            if (!selectedCharacters.Contains(selectedCharacterTexts[i].text))
+            {
+                characterButtons[i].color = new Color32(190, 104, 69, 255);  // 원래 색상으로 변경
             }
         }
     }
