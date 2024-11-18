792b54d342e9c88576695960c77819e995a52ab3
나예진 <nyj500@konkuk.ac.kr>
2024-11-18 15:46:09 +0900

    fix: enemyAgent script

--- Assets/Scripts/EnemyAgent.cs
+++ Assets/Scripts/EnemyAgent.cs
@@ -1,36 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
 using UnityEngine;
 
-public class EnemtAgent : Agent
+public class EnemyAgent : Agent
 {
+    public TileInfo currentTile;
+
+    // 에피소드 초기화
     public override void OnEpisodeBegin()
     {
-        // 에피소드 초기화 (필요에 따라 맵 초기화 등 추가)
-        Debug.Log("Episode Started");
+        AIManager.Instance.RandomDeploy();
     }
 
+    // 환경 관찰 수집
     public override void CollectObservations(VectorSensor sensor)
     {
-        // 간단한 관찰값 추가 (테스트용)
-        sensor.AddObservation(Random.Range(0f, 1f));  // 랜덤 값으로 테스트
-        Debug.Log("Observations Collected");
+        // 유닛의 현재 위치와 상태를 관찰
+        sensor.AddObservation(currentTile.unit.currentHealth);
+        sensor.AddObservation(currentTile.unit.currentAttackPoint);
+        sensor.AddObservation(currentTile.unit.currentDefensePoint);
+
+        // 주변 타일 상태
+        var nearbyTiles = MapManager.Instance.GetManhattanTiles(currentTile.gameObject, currentTile.unit.currentMoveRange);
+        foreach (var tile in nearbyTiles)
+        {
+            sensor.AddObservation(tile.GetComponent<TileInfo>().unit != null ? 1.0f : 0.0f);
+        }
     }
 
+    // 행동 정의
     public override void OnActionReceived(ActionBuffers actions)
     {
-        // 행동 수행 (테스트용)
         int action = actions.DiscreteActions[0];
-        Debug.Log($"Action Received: {action}");
+
+        if (action == 0)
+        {
+            TryMove();  // 이동
+        }
+        else if (action == 1)
+        {
+            TryAttack();  // 공격
+        }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        // 수동 테스트 (랜덤 행동 수행)
+        // 임시로 랜덤 행동 설정
         var discreteActions = actionsOut.DiscreteActions;
-        discreteActions[0] = Random.Range(0, 3);  // 3개의 행동 중 하나를 랜덤 선택
+        discreteActions[0] = Random.Range(0, 2); // 0: 이동, 1: 공격
+    }
+
+    private void TryMove()
+    {
+        // 현재 유닛 이동 로직 호출
+        AIManager.Instance.OnAITurnStarted();
+    }
+
+    private void TryAttack()
+    {
+        // 공격 수행 로직
+        AIManager.Instance.OnAITurnStarted();
     }
 }
